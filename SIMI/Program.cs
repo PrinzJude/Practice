1d21231a0c3fe0630062d89d2485c2bdab7620d8
PrinzJude <olumide792@gmail.com>
2021-07-12 22:29:27 +0100

    I added Simi Folder

---
+++ SIMI/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SIMI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Goodbye world");
+            int score = 10;
+            int time = 20;
+            Console.WriteLine{$"score + time"};
+
+
+        }
+    }
+}
